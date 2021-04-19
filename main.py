f22b41f236eba3ee61675b4c3a780a858c4a4d88
Youssef-Rachad <69124029+Youssef-Rachad@users.noreply.github.com>
2021-04-18 23:55:57 -0400

    added gif functionality + documentation

--- main.py
+++ main.py
@@ -5,35 +5,47 @@ import colorsys
 
 def scale_hue(r, g, b, factor):
     h, l, s = colorsys.rgb_to_hls(r, g, b)
+    # Return an rgb color based off of a scaled hue
+    # Light and Saturation are constant
     return colorsys.hls_to_rgb(h*factor, 0.54, 0.92)
 
 def barlist(nbr):
     return [random.randint(1, 10) for i in range(1, nbr)]
 
-colors=[]
 fig=plt.figure()
 
-n=1 #Number of frames
 #length of array to sort
-nbr = 50
+nbr = 20
+# generate indexes
 x=range(1, nbr)
-barcollection = plt.bar(x,barlist(nbr))
+# generate initial values
 y=barlist(nbr)
+# generate bar graph
+barcollection = plt.bar(x, y)
 #interval of time between each iteration
-delay = 250
+delay = 10
 
 def animate(i):
     for i, b in enumerate(barcollection):
+        # dynamically set height & color
         b.set_height(y[i])
         b.set_color(scale_hue(245/255, 29/255, 30/255, y[i]/max(y)))
+    # if out of order, then swap
     for j in range(len(y)-1):
         if y[j] > y[j+1]:
             y[j], y[j+1] = y[j+1], y[j]
+            # break to get one step at a time
             break
-    return
+#number of frames is max iteration (nbr^2) which is bbsort worst case
+anim=animation.FuncAnimation(fig,animate,repeat=True,blit=False,frames=(nbr*nbr),interval=delay)
 
-anim=animation.FuncAnimation(fig,animate,repeat=True,blit=False,frames=n,
-        interval=delay)
+# uncomment to show in real time
+#plt.show()
 
-plt.show()
+# Make a gif
 
+# Change the below path
+# note r"" means a raw string --> dont worry about backslashes
+f = r"c://path/to/file.gif"
+writergif = animation.PillowWriter(fps=5)
+anim.save(f, writer=writergif)
